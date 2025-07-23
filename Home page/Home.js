0218bc77de5dab0d8851e1ad2f733921691ea764
Sepideh Namavari <Sepideh.Namavari67@gmail.com>
2025-07-24 03:17:23 +0430

    edit 2

--- Home page/Home.js
+++ Home page/Home.js
@@ -1,41 +1,29 @@
 let currentScore = 0;
-// let username ;
+let username = "sepideh";
 let difficulty = null;
 let id = localStorage.getItem("userId")
 
-// function saveUserData(name, score) {
-//     localStorage.setItem("username", name);
-//     localStorage.setItem("score", score);
-// }
 
-window.onload = function() {
-    fetch(`https://68738976c75558e273547c3d.mockapi.io/users_info/${id}`) 
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
-        .then(data => {
+
+window.onload = async function() {
+        try{
+            const response = await fetch(`https://68738976c75558e273547c3d.mockapi.io/users_info/1`) 
+            const data = await response.json();
+            console.log(data);
             username = data.name;
             currentScore = parseInt(data.score) || 0;
-
-            // saveUserData(username, currentScore);
-
             document.getElementById("userId").textContent = username;
             document.getElementById("score").textContent = currentScore;
-
-        })
-        .catch(error => {
+        }
+         catch(error) {
             console.error("Fetch error:", error);
             alert("Could not fetch user data.");
 
             
-             username = localStorage.getItem("userId");
             currentScore = parseInt(localStorage.getItem("score")) || 0;
              document.getElementById("userId").textContent = username;
              document.getElementById("score").textContent = currentScore;
-        });
+        };
 };
     window.increaseScore = function(points) {
         currentScore += points;
@@ -47,7 +35,6 @@ window.onload = function() {
 function startGame() {
      difficulty = document.getElementById("difficulty").value;
     alert("Starting game at " + difficulty + " level");
-
 }
 
 document.querySelector(".start-btn").addEventListener("click",
